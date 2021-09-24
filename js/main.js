8faf0eecba862565f0c1957edbe82ddca60a1fba
veronnah <yoo.veroon@gmail.com>
2021-09-24 13:47:41 +0300

    responsive main page

--- js/main.js
+++ js/main.js
@@ -2,35 +2,56 @@ const mainSlider = new Swiper('.main__slider', {
   direction: 'vertical',
   loop: true,
   slidesPerView: 'auto',
-  spaceBetween: 210,
-  
+  autoplay: {
+    delay: 4000,
+  },
+  breakpoints: {
+    320: {
+      spaceBetween: 20
+    },
+    480: {
+      spaceBetween: 40,
+    },
+    1000: {
+      spaceBetween: 160,
+    },
+    1100: {
+      spaceBetween: 210,
+    }
+  },
+
 });
 const leftSlider = new Swiper('.left__slider', {
   direction: 'vertical',
   loop: true,
   slidesPerView: 4,
   spaceBetween: 30,
-  
+  autoplay: {
+    delay: 3000,
+  },
+
 });
 const rightSlider = new Swiper('.right__slider', {
   direction: 'vertical',
   loop: true,
   slidesPerView: 4,
   spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+  },
 });
 
 let cookiesBtn = document.querySelector('.cookies__btn');
 let cookies = document.querySelector('.cookies');
 
-cookiesBtn.addEventListener('click', () =>{
+cookiesBtn.addEventListener('click', () => {
   cookies.style.display = 'none';
 });
 
-let burgerBtn = document.querySelector('.burger'); 
-let burgerMenu = document.querySelector('.burger__menu'); 
+let burgerBtn = document.querySelector('.burger');
+let burgerMenu = document.querySelector('.burger__menu');
 
-burgerBtn.addEventListener('click', () =>{
-  burgerMenu.classList.toggle('active'); 
+burgerBtn.addEventListener('click', () => {
+  burgerMenu.classList.toggle('active');
   burgerBtn.classList.toggle('open');
 });
-
