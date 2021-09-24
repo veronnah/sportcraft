4f6d5faf229504928ab65876c85dbf7ad525150a
veronnah <yoo.veroon@gmail.com>
2021-09-24 17:02:09 +0300

    two pages finished:

--- js/main.js
+++ js/main.js
@@ -21,6 +21,26 @@ const mainSlider = new Swiper('.main__slider', {
   },
 
 });
+const mainSliderPage = new Swiper('.main__slider-page', {
+  direction: 'vertical',
+  loop: true,
+  slidesPerView: 'auto',
+  autoplay: {
+    delay: 4000,
+  },
+  breakpoints: {
+    320: {
+      spaceBetween: 20
+    },
+    480: {
+      spaceBetween: 40,
+    },
+    1100: {
+      spaceBetween: 50,
+    }
+  },
+
+});
 const leftSlider = new Swiper('.left__slider', {
   direction: 'vertical',
   loop: true,
@@ -31,7 +51,26 @@ const leftSlider = new Swiper('.left__slider', {
   },
 
 });
+const leftSliderPage = new Swiper('.left__slider-page', {
+  direction: 'vertical',
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+  },
+
+});
 const rightSlider = new Swiper('.right__slider', {
+  direction: 'vertical',
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+  },
+});
+const rightSliderPage = new Swiper('.right__slider-page', {
   direction: 'vertical',
   loop: true,
   slidesPerView: 4,
